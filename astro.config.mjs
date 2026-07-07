c9b77b3b33ef25fd6945d4c546722648635caccb
Robert Kezives <kezives3d@gmail.com>
2026-07-07 16:21:19 -0300

    fix: update base path to match GitHub Pages repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://r-kez.github.io',
-  base: '/k_tools_scene_light_manager_slm_documentation/',
+  base: '/k_tools_slm_documentation/',
   integrations: [tailwind()]
 });
